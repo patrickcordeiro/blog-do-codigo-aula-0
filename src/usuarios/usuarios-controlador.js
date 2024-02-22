ced47b46d0b91c062681c2a45aaab05fbdc70d4b
patrickcordeiro <patrick.cordeiro30@gmail.com>
2024-02-21 22:26:06 -0300

    feat: Utilizando bcrypt para fazer o hash das senhas

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -1,5 +1,5 @@
-const Usuario = require('./usuarios-modelo');
-const { InvalidArgumentError, InternalServerError } = require('../erros');
+const Usuario = require("./usuarios-modelo");
+const { InvalidArgumentError, InternalServerError } = require("../erros");
 
 module.exports = {
   adiciona: async (req, res) => {
@@ -9,8 +9,9 @@ module.exports = {
       const usuario = new Usuario({
         nome,
         email,
-        senha
       });
+
+      await usuario.adicionaSenha(senha);
 
       await usuario.adiciona();
 
@@ -39,5 +40,5 @@ module.exports = {
     } catch (erro) {
       res.status(500).json({ erro: erro });
     }
-  }
+  },
 };
